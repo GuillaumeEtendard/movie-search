401e814b293b1d301ac5f39cc0c02a54087668ba
GuillaumeEtendard <guillaumetendard@gmail.com>
2016-03-31 00:32:08 +0200

    Fix ajax

--- public/assets/js/search.js
+++ public/assets/js/search.js
@@ -12,37 +12,33 @@ $(function () {
                 var filmTable = $('#filmTable');
                 tableBody.html(filmTable);
 
-                for (var i in data.films) {
-                    S = data.films[i].duration;
-                    M = S / 60;
-                    S = S % 60;
-                    H = M / 60;
-                    M = M % 60;
+                function hours(secondsTime){
+                    var seconds = secondsTime;
+                    var minutes = seconds / 60;
+                    var hours = minutes / 60;
+                    minutes = minutes % 60;
+                    return Math.trunc(hours) + 'h' + Math.trunc(minutes)
+                }
 
+                for (var i in data.films) {
                     film = tableBody.append(
                         '<tr><td>' + data.films[i].title + '</td><br><br>' +
                         '<td>' + data.films[i].first_name+' '+data.films[i].last_name + '</td><br><br>' +
                         '<td>' + data.films[i].gender + '</td><br><br>' +
                         '<td>' + data.films[i].year + '</td><br><br>' +
                         '<td>' + data.films[i].synopsis + '</td><br><br>' +
-                        '<td>' + Math.trunc(H) + 'h' + Math.trunc(M) + '</td><br><br></tr>'
+                        '<td>' + hours(data.films[i].duration) + '</td><br><br></tr>'
                     );
-
                 }
-
             },
-
             error: function (data, status, error) {
                 var showErrors = '';
                 data = JSON.parse(data.responseText);
                 for (var d in data.errors) {
                     showErrors += d + ' :' + data.errors[d] + '<br>';
                 }
-                $('#errorBlock').html(showErrors);
             }
         });
         return false;
-
     });
-
 });
